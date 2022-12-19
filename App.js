237829fb916c3318297e7af8d17080450acd95c8
lkulier <lkulier@foi.hr>
2022-12-19 17:09:08 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  function Zbroji(e) {
+    return setCount((prevCount) => prevCount + 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+      <Text>{count}</Text>
+      <Button title="Test" onPress={(e) => Zbroji(e)} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
